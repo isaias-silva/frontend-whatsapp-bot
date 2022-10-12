0a524ea4ee716901f582cf1f1a8fd37ede15afa6
higordevv <pedrohigor2004@gmail.com>
2022-10-12 20:59:35 -0300

    Mudanças a serem submetidas: 	deleted:    src/components/qrcode/hook/useQR.ts 	deleted:    src/components/qrcode/index.d.ts 	modified:   src/components/qrcode/index.tsx 	deleted:    src/service/hooks/useSocket.ts

--- src/components/qrcode/index.d.ts
+++
@@ -1,7 +0,0 @@
-import 'react-qr-code'
-
-declare module 'react-qr-code'{
-    export interface QRCodeProps{
-        value: string
-    }
-}
--- src/components/qrcode/index.tsx
+++ src/components/qrcode/index.tsx
@@ -1,28 +1,59 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import QRCode from 'react-qr-code'
 import AnimationLoading from '../../animations/loadingAnimation'
-import useQr from './hook/useQR'
+import { io } from 'socket.io-client'
 
 interface IQr {
   status?: string
-  qr: undefined | {
-    qr: string
+  qr:
+    | undefined
+    | {
+        qr: string
+      }
+}
+
+function Fetch<T>(): [boolean, T | undefined, string, () => void] {
+  const [data, setData] = useState<T>()
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(' ')
+  const socket = io('ws://localhost:8081/')
+
+  const sendReq = () => {
+    socket.on('connect', () => {
+      socket.emit('controlstart', 'qualquercoisa')
+      setLoading(true)
+      setError(' ')
+    })
+    socket.on('conn', (obj) => {
+      setData(obj)
+      console.log(obj)
+      setLoading(false)
+    })
   }
+  const handleFetch = () => {
+    sendReq()
+  }
+
+  return [loading, data, error, handleFetch]
 }
 
 function QRcode() {
   const [qr, setQr] = useState<string>(' ')
+  const [loading, data, error, handleFetch] = Fetch<IQr>()
 
-  const [data, loading, error] = useQr<IQr>('ws://localhost:8081/')
-
-  
-  useEffect(() => {
-    if (data?.qr?.qr) setQr(String(data.qr.qr))
+  const handleQrcode = useCallback(() => {
+    if (data?.qr?.qr) {
+      setQr(String(data.qr.qr))
+    }
   }, [data])
 
+  useEffect(() => {
+    handleFetch()
+    handleQrcode()
+  })
   return (
     <div style={{ height: '700', margin: 'auto', width: '500', background: 'white' }}>
-      <QRCode size={300} value={qr} />
+      {qr ? <QRCode value={qr} /> : error}
     </div>
   )
 }
